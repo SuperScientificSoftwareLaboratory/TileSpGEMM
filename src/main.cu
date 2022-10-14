dcab0d0cf8d4d9c8722e6a4cc7a1bb5fc434e074
Elvir Crncevic <elvircrn@gmail.com>
2022-10-14 18:02:57 +0200

    Fixed a use after free crash when -D CHECK_RESULT=1 is used

--- src/main.cu
+++ src/main.cu
@@ -177,9 +177,9 @@ printf("tile space overhead = %.2f MB\n", mem);
 
         csr2tile_col_major(matrixB);
 
-        free(matrixA->rowpointer);
-        free(matrixA->columnindex);
-        free(matrixA->value);
+        // free(matrixA->rowpointer);
+        // free(matrixA->columnindex);
+        // free(matrixA->value);
 
         int blk_intersec_bitmask_len = ceil((double)matrixA->tilen / 32.0);
         double densityA = (double)matrixA->numtile / ((double)matrixA->tilem*(double)matrixA->tilen);
